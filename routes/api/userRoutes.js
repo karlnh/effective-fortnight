2b693c26956b717408e0a595d2352a3d0205ef62
karlnh <hubigkarl@gmail.com>
2023-03-11 16:15:39 -0600

    create user routing

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,30 @@
 const router = require('express').Router();
 
+const {
+    getAllUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    createFriend,
+    deleteFriend,
+} = require('../../controllers/userController');
+
+// /api/users/
+router.route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// /api/users/:userId/
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// /api/users/:userId/friends/
+router.route('/:userId/friends').post(createFriend);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').delete(deleteFriend);
+
 module.exports = router;
