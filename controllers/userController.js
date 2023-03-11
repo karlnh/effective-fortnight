d821c0203d86ebeb6a6560424871293a0c464629
karlnh <hubigkarl@gmail.com>
2023-03-11 16:24:15 -0600

    init user controller

---
+++ controllers/userController.js
@@ -0,0 +1,31 @@
+const User = require('../models/User');
+
+module.exports = {
+    getAllUsers(req, res) {
+
+    },
+
+    getSingleUser(req, res) {
+
+    },
+
+    createUser(req, res) {
+
+    },
+
+    updateUser(req, res) {
+
+    },
+
+    deleteUser(req, res) {
+
+    },
+
+    createFriend(req, res) {
+
+    },
+
+    deleteFriend(req, res) {
+
+    },
+};
