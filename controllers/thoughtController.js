201cc8065f256d658d03151f2aa2ed0975754f5b
karlnh <hubigkarl@gmail.com>
2023-03-11 16:25:57 -0600

    create thought controller (1984)

---
+++ controllers/thoughtController.js
@@ -0,0 +1,31 @@
+const Thought = require('../models/Thought');
+
+module.exports = {
+    getAllThoughts(req, res) {
+
+    },
+
+    getSingleThought(req, res) {
+
+    },
+
+    createThought(req, res) {
+
+    },
+
+    updateThought(req, res) {
+
+    },
+
+    deleteThought(req, res) {
+
+    },
+
+    createReaction(req, res) {
+
+    },
+
+    deleteReaction(req, res) {
+
+    },
+};
