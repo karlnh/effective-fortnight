530021ab2547e56d086acdbb4fbeeb916baa687f
karlnh <hubigkarl@gmail.com>
2023-03-14 23:02:50 -0500

    create friend

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,20 @@ module.exports = {
             .catch((err) => res.status(500).json(err));
     },
     createFriend(req, res) {
-        res.send(`Creating new friend at user ID ${req.params.userId}...`);
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.body } },
+            { runValidators: true, new: true }
+        )
+        .then((user) =>
+            !user
+                ? res.status(404).json({ message: "No friend found with this ID"})
+                : res.json(user)
+        )
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
     },
     getUsers(req, res) {
         User.find()
