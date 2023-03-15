03197ee90763f805c1f80cf5762d208b1363406e
karlnh <hubigkarl@gmail.com>
2023-03-14 22:02:50 -0500

    reformat

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
-const Reaction = require('./Reaction');
-const Thought = require('./Thought');
 const User = require('./User');
+const Thought = require('./Thought');
+const Reaction = require('./Reaction');
 
-module.exports = { Reaction, Thought, User };
+
+module.exports = { User, Thought, Reaction };
