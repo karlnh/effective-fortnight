f35939f3683a14458cbd24e139182857ba763163
karlnh <hubigkarl@gmail.com>
2023-03-14 22:02:28 -0500

    update thought routing

---
+++ controllers/thoughtController.js
@@ -0,0 +1,26 @@
+const Thought = require('../models/Thought');
+const Reaction = require('../models/Reaction');
+
+module.exports = {
+    createThought(req, res) {
+        res.send(`Creating thought with your request: ${req.body}`);
+    },
+    createReaction(req, res) {
+        res.send(`Creating reaction at thought ${req.params.thoughtId}...`);
+    },
+    getThoughts(req, res) {
+        res.send(`Finding all thoughts...`);
+    },
+    getSingleThought(req, res) {
+        res.send(`Finding thought by ID ${req.params.thoughtId}...`);
+    },
+    updateThought(req, res) {
+        res.send(`Updating thought by ID ${req.params.thoughtId}...`);
+    },
+    deleteThought(req, res) {
+        res.send(`Deleting thought by ID ${req.params.thoughtId}...`);
+    },
+    deleteReaction(req, res) {
+        res.send(`Deleting reaction by ID ${req.params.reactionId} at thought ID ${req.params.thoughtId}...`);
+    }
+};
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,29 +1,28 @@
 const router = require('express').Router();
-const { 
-    getAllThoughts,
-    getSingleThought,
+
+const {
     createThought,
+    createReaction,
+    getThoughts,
+    getSingleThought,
     updateThought,
     deleteThought,
-    createReaction,
-    deleteReaction,
-} = require('../../controllers/thoughtController.js');
+    deleteReaction
+} = require('../../controllers/thoughtController');
 
-// /api/thoughts
 router.route('/')
-    .get(getAllThoughts)
-    .post(createThought);
+    .post(createThought)
+    .get(getThoughts);
 
-// /api/thoughts/:thoughtId
 router.route('/:thoughtId')
     .get(getSingleThought)
     .put(updateThought)
     .delete(deleteThought);
 
-// /api/thoughts/:thoughtId/reactions
-router.route('/:thoughtId/reactions').post(createReaction);
+router.route('/:thoughtId/reactions')
+    .post(createReaction);
 
-// /api/thoughts/:thoughtId/reactions/:reactionId
-router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+router.route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction);
 
 module.exports = router;
