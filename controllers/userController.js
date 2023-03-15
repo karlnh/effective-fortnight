433e06efa22014b353f2a4f9c48213167ea82095
karlnh <hubigkarl@gmail.com>
2023-03-14 23:32:54 -0500

    import thought model; add some CRUD

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const { User, Thought } = require('../models');
 
 // Create-Read-Update-Delete
 
@@ -40,12 +40,45 @@ module.exports = {
             .catch((err) => res.status(500).json(err));
     },
     updateUser(req, res) {
-        res.send(`Updating user by ID ${req.params.userId}...`);
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .then((user) =>
+            !user
+                ? res.status(404).json({ message: 'No user found with this ID'})
+                : res.json(user)
+        )
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        })
     },
     deleteUser(req, res) {
-        res.send(`Deleting user and associated thoughts by ID ${req.params.userId}...`);
+        // res.send(`Deleting user and associated thoughts by ID ${req.params.userId}...`);
+        User.findOneAndRemove({ _id: req.params.userId })
+        .then((user) =>
+            !user
+                ? res.status(404).json({ message: "No user found with this ID"})
+                : res.json(`Successfully deleted user ${req.params.userId}` )
+        )
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
     },
     deleteFriend(req, res) {
-        res.send(`Deleting friend ${req.params.friendId} at user ID ${req.params.userId}...`);
-    }
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: { friendId: req.params.friendId } } },
+            { runValidators: true, new: true }
+        )
+        .then((user) =>
+            !user
+                ? res.status(404).json({ message: "No user found with this ID"})
+                : res.json(`Successfully deleted friend ${req.params.friendId} at user ${req.params.userId}`)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
 };
