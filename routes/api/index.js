c44918867acc8d0c718e25f656af914ee6c3b24b
karlnh <hubigkarl@gmail.com>
2023-03-14 22:03:16 -0500

    add basic testing to route indexes

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,4 +5,8 @@ const thoughtRoutes = require('./thoughtRoutes');
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
 
+router.get("/", (req, res) => {
+    res.send("Connected to API root.");
+});
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,8 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+router.get("/", (req, res) => {
+    res.send("Connected to home route.");
+});
+
 module.exports = router;
