02276266d6e0dc533801fc73a54c88ffeac8848c
karlnh <hubigkarl@gmail.com>
2023-03-14 23:57:44 -0500

    change seed to only users

--- utils/seed.js
+++ utils/seed.js
@@ -1,6 +1,5 @@
 const connection = require('../config/connection');
 const { User, Thought } = require('../models');
-const { getRandomThoughts, getRandomUsername, getRandomEmail } = require('./data')
 
 connection.on("error", (err) => err);
 
@@ -15,32 +14,26 @@ connection.once("open", async () => {
     {
         username: "catchable7444",
         email: "nearness@imagines.org",
-        thoughts: "Hello world",
     },
     {
         username: "comment6145",
         email: "fencing@turtle.io",
-        thoughts: "Hunan style beef stir fry",
     },
     {
         username: "wildly3845",
         email: "reversing@giggling.net",
-        thoughts: "Are you gonna eat that",
     },
     {
         username: "thaw8153",
         email: "purity@imagines.gov",
-        thoughts: "The ship has set sail",
     },
     {
         username: "molecular0914",
         email: "viability@turtle.cern",
-        thoughts: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     },
     {
         username: "plural7060",
         email: "squatter@diameter.cafe",
-        thoughts: "KitKat Dark King Size Net Wt 3 Oz",
     },
 );
 
